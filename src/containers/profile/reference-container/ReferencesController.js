c4efc2eebcc31d87b06aeec49ec2a2268efa364c
“Lige-w” <Lige.williamson@gmail.com>
2019-06-28 17:34:57 -0400

    renamed resources to references

--- src/containers/profile/reference-container/ReferencesController.js
+++ src/containers/profile/reference-container/ReferencesController.js
@@ -3,6 +3,7 @@ import React from 'react'
 const ResourcesController = () => {
     return (
         <div id="resources-controller">
+            <NewReference></NewReference>
         </div>
     )
 }
