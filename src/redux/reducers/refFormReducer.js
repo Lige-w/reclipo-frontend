2de6c65b27da42c789956d489876e1522534864e
“Lige-w” <Lige.williamson@gmail.com>
2019-07-05 09:24:18 -0400

    add form reducer

---
+++ src/redux/reducers/refFormReducer.js
@@ -0,0 +1,10 @@
+const refFormReducer = (state = false, action) => {
+    switch(action.type) {
+        case 'SET_IS_SHOWING_REF_FORM':
+            return action.boolean
+        default:
+            return state
+    }
+}
+
+export {refFormReducer}
